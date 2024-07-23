5235e2c85d53f7504da1185f1cba7349527147a2
Sharmila-Rajendran <139245927+Sharmila-Rajendran@users.noreply.github.com>
2024-07-23 17:16:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function Click(){
+    document.getElementsByClassName("val")[0].innerHTML='Background Color : Red';
+    document.getElementsByClassName("container")[0].style.backgroundColor='red';
+}
+
+function yellow(){
+    document.getElementsByClassName("val")[0].innerHTML='Background Color : Yellow';
+    document.getElementsByClassName("container")[0].style.backgroundColor='yellow';
+}
+
+function Purple(){
+    document.getElementsByClassName("val")[0].innerHTML='Background Color : Purple';
+    document.getElementsByClassName("container")[0].style.backgroundColor='purple';
+}
+
+function green(){
+    document.getElementsByClassName("val")[0].innerHTML='Background Color : Green';
+    document.getElementsByClassName("container")[0].style.backgroundColor='green';
+}
